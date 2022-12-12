105ea0c1a04582fcea02cfd766bb8a2de5c7a594
MrOmarxD <omareiyana@gmail.com>
2022-12-12 13:42:09 +0100

    /

---
+++ Angular/appHolaMundo/src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  // template: 'Prueba de template',
+  styles: []
+})
+export class HeaderComponent implements OnInit{
+  constructor(){}
+  ngOnInit(){
+
+  }
+}
