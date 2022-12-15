0f9daf575d6a62d7633277d66297105366f333df
MrOmarxD <omareiyana@gmail.com>
2022-12-15 12:39:43 +0100

    15/12/22 Clase Pag 30 Heroes

--- Angular/appHeroes/src/app/app.module.ts
+++ Angular/appHeroes/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { APP_ROUTING } from './app.routes';
 
 //Servicios
+import { HeroesService } from './servicios/heroes.service';
 
 //Componentes
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { AboutComponent } from './components/about/about.component';
+// import { Services } from '@angular/core/src/view';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,7 @@ import { AboutComponent } from './components/about/about.component';
     //HttpModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Angular/appHeroes/src/app/components/heroes/heroes.component.ts
+++ Angular/appHeroes/src/app/components/heroes/heroes.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from 'src/app/servicios/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit{
 
+  heroes: Heroe[] = [];
+  constructor(private _heroesService: HeroesService){}
+
+  //Esta funcion se ejecutara cuando este toda la pagina cargada
+  ngOnInit(){
+    this.heroes = this._heroesService.getHeroes();
+
+    console.log(this.heroes);
+  }
 }
